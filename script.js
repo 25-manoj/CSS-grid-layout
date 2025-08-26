97d22b6abd76b0826e46a4c154a5bf7898b4d456
25-manoj <manojwebdevelop@gmail.com>
2025-08-26 16:23:32 +0530

    art challenge update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+
+let currentPath = window.location.pathname;
+
+  const navLinks = document.querySelectorAll('nav a');
+
+  navLinks.forEach(link => {
+
+    if (link.getAttribute('href') === currentPath) {
+      link.classList.add('active'); // Add active class
+    }
+  });
+
+console.log(currentPath)
